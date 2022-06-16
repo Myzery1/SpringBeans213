9f7e438c4d949199f5e97e38ad613de81bdc42cf
Myzery1 <myzery38@gmail.com>
2022-06-16 20:30:50 +0700

    Dog class added, timer wired

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,32 +1,32 @@
-//import app.config.AppConfig;
-//import app.model.AnimalsCage;
-//import org.junit.Assert;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.ApplicationContext;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = AppConfig.class)
-//public class MainTest {
-//
-//    @Autowired
-//    private ApplicationContext applicationContext;
-//
-//    @Test
-//    public void mainTest() {
-//        long time = 0;
-//        for (int i = 0; i < 5; i++) {
-//            AnimalsCage bean =
-//                    applicationContext.getBean(AnimalsCage.class);
-//            if (i == 0) {
-//                time = bean.getTimer().getTime();
-//                continue;
-//            }
-//            Assert.assertEquals("Ð¢ÐµÑÑ‚ Ð¿Ñ€Ð¾Ð²Ð°Ð»ÐµÐ½, Ð½Ðµ ÐºÐ¾Ñ€Ñ€ÐµÐºÑ‚Ð½Ð°Ñ Ñ€ÐµÐ°Ð»Ð¸Ð·Ð°Ñ†Ð¸Ñ Ð±Ð¸Ð½Ð¾Ð².", time, bean.getTimer().getTime().longValue());
-//            System.out.println(time);
-//        }
-//    }
-//}
+import app.config.AppConfig;
+import app.model.AnimalsCage;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
+public class MainTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    public void mainTest() {
+        long time = 0;
+        for (int i = 0; i < 5; i++) {
+            AnimalsCage bean =
+                    applicationContext.getBean(AnimalsCage.class);
+            if (i == 0) {
+                time = bean.getTimer().getTime();
+                continue;
+            }
+            Assert.assertEquals("Òåñò ïðîâàëåí, íå êîððåêòíàÿ ðåàëèçàöèÿ áèíîâ.", time, bean.getTimer().getTime().longValue());
+            System.out.println(time);
+        }
+    }
+}
